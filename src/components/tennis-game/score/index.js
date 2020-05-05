0dc0228ab0899e8a251e313e22beb1b8df8ce93f
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-05-05 16:29:10 +0530

    refactor: Update hardcoded value

--- src/components/tennis-game/score/index.js
+++ src/components/tennis-game/score/index.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Constants from "../../../constant";
 
 const Score = () => {
+  const [score, setScore] = useState(0);
   return (
     <div>
       <h5>{Constants.SCORE}</h5>
-      <label>0</label>
+      <label>{score}</label>
     </div>
   );
 };
