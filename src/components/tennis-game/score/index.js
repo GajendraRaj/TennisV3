84cde3e4a6ddeb031b9b671f76b4b670781519ff
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-05-06 12:25:04 +0530

    refactor: Simplify the update score logic

--- src/components/tennis-game/score/index.js
+++ src/components/tennis-game/score/index.js
@@ -3,29 +3,22 @@ import PropTypes from "prop-types";
 import Constants from "../../../constant";
 
 const Score = (props) => {
+  const SCORE_LOOKUP = ["0", "15", "30", "40"];
   const [score, setScore] = useState("0");
   const { player1Score, player2Score } = props;
 
   useEffect(() => {
-    let gameScore;
-    if (player1Score === 1) {
-      gameScore = `15, 0`;
-    } else if (player1Score === 2) {
-      gameScore = `30, 0`;
-    } else if (player1Score === 3) {
-      gameScore = `40, 0`;
-    } else if (player2Score === 1) {
-      gameScore = `0, 15`;
-    } else if (player2Score === 2) {
-      gameScore = `0, 30`;
-    } else if (player2Score === 3) {
-      gameScore = `0, 40`;
-    } else {
-      gameScore = "0";
-    }
+    const gameScore = () => {
+      const isPlayersScoreEqual = player1Score === player2Score;
+      if (!isPlayersScoreEqual) {
+        return SCORE_LOOKUP[player1Score] + ", " + SCORE_LOOKUP[player2Score];
+      } else {
+        return SCORE_LOOKUP[0];
+      }
+    };
 
     setScore(gameScore);
-  }, [player1Score, player2Score]);
+  }, [player1Score, player2Score, SCORE_LOOKUP]);
 
   return (
     <div>
